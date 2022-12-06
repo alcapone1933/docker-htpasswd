2c2ffd8fce2c1c33901c6e919cec9075ce130f40
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-12-06 01:21:56 +0100

    „entrypoint.sh“ ändern

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 sleep 5
-htpasswd -Bbn $USER_NAME $USER_PASSWD > /data/htpasswd
+echo -n "" > /data/htpasswd
+echo > /data/htpasswd && echo >> /data/htpasswd
+echo "YOUR PASSWORD: `htpasswd -Bbn $USER_NAME $USER_PASSWD`" >> /data/htpasswd && echo >> /data/htpasswd
+# htpasswd -Bbn $USER_NAME $USER_PASSWD >> /data/htpasswd
 sleep 2
 cat /data/htpasswd
